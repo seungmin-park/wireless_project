c062651e782ac199dbde60c21cda9b98c0981f68
박승민 <tmddudals369@naver.com>
2022-12-01 17:33:33 +0900

    feat: 사람 구별하여 LED 점등 기능 구현

---
+++ arduino/RevolvingDoor/RevolvingDoor.ino
@@ -0,0 +1,38 @@
+#define LED_R 5
+#define LED_Y 4
+#define LED_G 3
+ 
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(LED_R, OUTPUT);
+  pinMode(LED_Y, OUTPUT);
+  pinMode(LED_G, OUTPUT);
+}
+ 
+void loop() {
+  // digitalWrite(LED_R, HIGH);
+  if(Serial.available()){
+    char isDetectChair = Serial.read();
+    if(isDetectChair == 'p'){
+      digitalWrite(LED_R, HIGH);
+      turnOffAll();
+    }
+    else if(isDetectChair == 'n'){
+      digitalWrite(LED_Y, HIGH);
+      turnOffAll();
+    }
+  }
+}
+ 
+void turnOffAll() {
+  digitalWrite(LED_R, LOW);
+  digitalWrite(LED_Y, LOW);
+  digitalWrite(LED_G, LOW);
+}
+ 
+void turnOnAll() {
+  digitalWrite(LED_R, HIGH);
+  digitalWrite(LED_Y, HIGH);
+  digitalWrite(LED_G, HIGH);
+}
